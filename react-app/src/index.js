c347c6f8405b53b0db5b9ae1811db5a5a812e9aa
LoJo <louis.low.2018@sis.smu.edu.sg>
2020-04-14 23:40:31 +0800

    Hello World Step of creating React App

---
+++ react-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello World</h1>; // Babel will compile this down to React.createElement
+
+
+// Note that React server has hot moduling -> like Flask for Python when debug mode = true
+//console.log(element)
+
+// first argument is the component we want to render on JSX side and the second argument is the vanilla way of getting the 
+// id of the div container that we want to render the React Component in the DOM.
+ReactDOM.render(element, document.getElementById('root'));
+
