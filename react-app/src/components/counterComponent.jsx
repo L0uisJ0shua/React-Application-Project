dcba2e698bb35fc9d0720da8f0fa61a3289ed96c
LoJo <louis.low.2018@sis.smu.edu.sg>
2020-04-16 00:20:52 +0800

    Created Vidly project
    Will attempt to create the actual App tomorrow

--- react-app/src/components/counterComponent.jsx
+++ react-app/src/components/counterComponent.jsx
@@ -14,22 +14,38 @@ class Counter extends Component {
 		fontWeight: "bold",
 	};
 	// using styles attribute in React App
-	// or we can just pass style = {{ fontSize : 10 }} in the span
+  // or we can just pass style = {{ fontSize : 10 }} in the span
+  
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this); // allows our handleIncrement to refer to a Counter Object always
+  // }
 
-  // return null if we do not want to display anything
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-    return (
+	// return null if we do not want to display anything
+	renderTags() {
+		if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+		return (
 			<ul>
 				{this.state.tags.map((tag) => (
 					<li key={tag}>{tag}</li>
 				))}
 			</ul>
 		);
-  }
+	}
 
+  // Arrow Function can be used for binding of Event Handler Functions as well
+	handleIncrement = () => {
+    this.setState({ count : this.state.count+1});
+    // using a method inherited from React Component, we can setState of the virtual DOM at then React will bring the DOM 
+    // to the new state of the Virtual DOM
 
+    //obj.method(); --> referst to the object
+    // function(); --> refers to window object which in strict mode is undefined
+	}
+
+  // asynchornous call
 	render() {
 		// React.createElement('div');
 
@@ -39,14 +55,19 @@ class Counter extends Component {
 				{/* <span style={this.styles}> Welcome to Counter </span>  */}
 
 				<span className={this.getBadgeStatus()}> {this.formatCount()} </span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
-        { this.state.tags.length === 0 && 'Please create a new tag'}
+				<button
+					onClick={this.handleIncrement}
+					className="btn btn-secondary btn-sm"
+				>
+					Increment
+				</button>
+				{this.state.tags.length === 0 && "Please create a new tag"}
 				{this.renderTags()}
 			</React.Fragment>
 		);
 		// React.Fragment is child in React that we can use to hide the div and wrap our tags inside so that Babel is able to compile it
-    // Whenever you render using a map method, we need to set a keyAttribute to tell React which virtual DOM changed
-    // JavaScript is able to evaluate a combined boolean and non-boolean of && conditions therefore, it is able to return it as a string
+		// Whenever you render using a map method, we need to set a keyAttribute to tell React which virtual DOM changed
+		// JavaScript is able to evaluate a combined boolean and non-boolean of && conditions therefore, it is able to return it as a string
 	}
 
 	getBadgeStatus() {
