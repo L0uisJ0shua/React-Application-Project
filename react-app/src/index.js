1964f6c124439cf804506060d7447cf34174f934
LoJo <louis.low.2018@sis.smu.edu.sg>
2020-04-15 22:31:41 +0800

    Added my First React Component
    Learnt how to create a React Component and Display on web page

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counterComponent";
 
-const element = <h1>Hello World</h1>; // Babel will compile this down to React.createElement
+// const element = <h1>Hello World</h1>; // Babel will compile this down to React.createElement
+
 
 
 // Note that React server has hot moduling -> like Flask for Python when debug mode = true
@@ -9,5 +12,6 @@ const element = <h1>Hello World</h1>; // Babel will compile this down to React.c
 
 // first argument is the component we want to render on JSX side and the second argument is the vanilla way of getting the 
 // id of the div container that we want to render the React Component in the DOM.
-ReactDOM.render(element, document.getElementById('root'));
+// ReactDOM.render(element, document.getElementById('root'));
 
+ReactDOM.render(<Counter />, document.getElementById('root'));
